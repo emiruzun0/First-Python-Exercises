0de259a52ba55e22fe31b27fed9bff12ff702eb4
Emirhan Uzun <emirhan_uzun_1907@hotmail.com>
2020-07-04 19:15:49 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -5,4 +5,4 @@ message = "Hello World!"
 print(message[:5])  #print from 5.index to end
 print(message[2:5]) #print from 2.index to 5.index
 
-newMessage =
+newMessage = message[:5] #substring of message from 5.index to end
