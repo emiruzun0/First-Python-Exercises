0de1903087e20c882e1ad45caced7609a67e3c66
Emirhan Uzun <emirhan_uzun_1907@hotmail.com>
2020-10-01 00:30:04 +0300

    Create functions2.py
    Return values and floor division

---
+++ functions2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 23:52:09 2020
+
+@author: Emirhan UZUN
+
+#Functions2
+"""
+
+def area_triangle(base,height):
+    return base*height/2 #return the value
+
+
+area_b = area_triangle(5, 4)
+area_a = area_triangle(7, 3)
+
+sum = area_a + area_b
+print("The sum of both areas is : " + str(sum))
+
+#---------------------------------
+
+def convert_seconds(seconds):
+    hours = seconds // 3600   # "//" A floor division divides a number and takes the integer part of the division as the result.
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600- minutes * 60 
+    return hours,minutes,remaining_seconds
+
+hours, minutes , seconds = convert_seconds(5000)
+print(hours,minutes,seconds)
